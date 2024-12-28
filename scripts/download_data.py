2a059c5715e742c1e993798b0ea5c1a0257573a5
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-12-28 15:30:45 +0000

    Rework dowload process

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,8 +1,11 @@
-# ./scripts/download_data.py
-import tensorflow as tf
+# ./utils/download_data.py
 import os
+import logging
 import numpy as np
+import tensorflow as tf
 
+
+logger = logging.getLogger(__name__)
 
 def download_mnist_data(dataset_path='./data/MNIST'):
     if not os.path.exists(dataset_path):
@@ -11,27 +14,20 @@ def download_mnist_data(dataset_path='./data/MNIST'):
     file_path = os.path.join(dataset_path, 'mnist.npz')
     url = 'https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz'
 
-    # Download the dataset using tf.keras.utils.get_file
+    logger.info('Downloading MNIST data...')
     tf.keras.utils.get_file(
         fname='mnist.npz',
         origin=url,
         cache_dir=dataset_path,
         cache_subdir='.',
     )
+    logger.info(f"Data downloaded to: {file_path}")
 
-    # Load the dataset from the downloaded file
     with np.load(file_path, allow_pickle=True) as f:
         x_train, y_train = f['x_train'], f['y_train']
         x_test, y_test = f['x_test'], f['y_test']
 
-    print('Data downloaded to:', dataset_path)
-    print(f"Number of training samples: {x_train.shape[0]}")
-    print(f"Number of training labels: {y_train.shape[0]}")
-    print(f"Number of test samples: {x_test.shape[0]}")
-    print(f"Number of test labels: {y_test.shape[0]}")
+    logger.info(f"Number of training samples: {x_train.shape[0]}")
+    logger.info(f"Number of test samples: {x_test.shape[0]}")
 
     return (x_train, y_train), (x_test, y_test)
-
-
-if __name__ == '__main__':
-    download_mnist_data()
